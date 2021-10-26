ef7f1a067f637968d4bb497fd8d0721ad366b115
Andrew Paige <keeperpaige@gmail.com>
2021-10-26 18:49:31 -0400

    intial setup

--- app.py
+++ app.py
@@ -20,27 +20,39 @@ class User(db.Model):
 
 @app.route('/')
 def index():
-    if 'user' in session:
-        return "Logged in as " + session['user']
+    if 'email' in session:
+        user = User.query.filter_by(email=session["email"]).first()#find the first user that matches name
+        return render_template("home.html", user=user)
     return render_template('index.html')
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if 'email' in session:
+        return redirect(url_for('index'))
     if request.method == 'POST':#handle form
         email = request.form['email']#get username from form
         existing_user = User.query.filter_by(email=email).first()#find the first user that matches name
         if existing_user:#check if user exists
             #hash the password and compare it to the one stored in the db
             existing_pass = existing_user.password
-            if bcrypt.checkpw(request.form['pass'].encode('utf-8'), existing_pass.encode('utf-8')):
+            if bcrypt.checkpw(request.form['pass'].encode('utf-8'), existing_pass):
                 #create session
-                print('bro??')
-                session['user'] = request.form['email']
+                session['email'] = request.form['email']
                 return redirect(url_for('index'))
+            else:
+                print('wrong username or password')
         else:
             print('wrong username or password')
     return render_template("login.html")
 
+@app.route('/logout', methods=['POST'])
+def logout():
+    session.clear()
+    return redirect(url_for('index'))
+
+@app.before_request
+def make_session_permanent():
+    session.permanent = True
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
@@ -60,12 +72,30 @@ def register():
             db.session.add(user)
             #commit new user to table
             db.session.commit()
-            session['user'] = email
+            session['email'] = email
             print('user added')
             return redirect(url_for('index'))
         else:
             print('user exists')
     return render_template("register.html")
+
+
+@app.route("/profile")
+def profile():
+    return render_template("profile.html")
+
+
+@app.route("/classes")
+def classes():
+    return render_template("classes.html")
+
+@app.route("/events")
+def events():
+    return render_template("events.html")
+
+@app.route("/connect")
+def connect():
+    return render_template("connect.html")
 
 if __name__ == '__main__':
   app.run(debug=True)
